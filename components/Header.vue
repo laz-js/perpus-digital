4ec1e35a36f3d61b1f9846714a71e662e47c5097
Afkar-Saga <afkarsukmawanahmad@gmail.com>
2023-10-06 15:59:44 +0700

    nuxt start

---
+++ components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="2xl:container mx-auto py-10 text-center bg-cyan-200">
+        <h1 class="text-4xl font-serif font-extrabold">Perpus Digital</h1>
+        <p class="text-sm font-light">Catatan Kunjungan Perpustakaan SMKN 4 Tasikmalaya</p>
+    </div>
+</template>
